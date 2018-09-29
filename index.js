ce93a61143154e2ddef81529da8ab76ef2004b39
iHype <42493144+iHype@users.noreply.github.com>
2018-09-29 21:54:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ mybot.on("ready", async ready =>{
 mybot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return message.channel.send("No DM's");
-  if(message.content.replace(/[^<]/g, "").length >= 10 && message.content.replace(/[^>]/g, "").length >= 10){
+  if(message.content.replace(/[^<]/g, "").length >= 5 && message.content.replace(/[^>]/g, "").length >= 5){
     let lol = message.guild.members.find('id',message.author.id);
     lol.addRole(message.guild.roles.find('name','muted').id);
-    message.channel.send('No mass mention').then(t => {t.delete(2500)})
-    message.delete(10);
+    message.channel.send('No mass mention').then(t => {await t.delete(2500)})
+    await message.delete(10);
   }
   if(!sentMessage[message.author.id]) {
     sentMessage[message.author.id] = {
