1f5c69fa9b0ceb8262981f8886bd017ae8b39347
iHype <42493144+iHype@users.noreply.github.com>
2018-09-29 15:48:07 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+var mybot = new Discord.Client();
+let sentMessage = {}
+mybot.on("ready", async ready =>{
+  console.log("ready");
+})
+mybot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return message.channel.send("No DM's");
+  if(!sentMessage[message.author.id]) {
+    sentMessage[message.author.id] = {
+    sent: 0
+    }
+  }
+  sentMessage[message.author.id].sent = sentMessage[message.author.id].sent + 1;
+  setTimeout( function() {  sentMessage[message.author.id].sent = sentMessage[message.author.id].sent - 1;}, 2000)
+let userMessage = message.guild.members.find("id", message.author.id)
+    if(message.content === "ping") {
+        message.channel.send("pong");
+    }
+let muteroleid = message.guild.roles.find("name", "muted").id;
+  if(sentMessage[message.author.id].sent === 4) {
+    let cyber = message.guild.members.find("id", "299495028756054016")
+    let hyper = message.guild.members.find("id", "430447525800181762")
+    let ibrahim = message.guild.members.find("id", "453970692266786816")
+    let embedTimeout = new Discord.RichEmbed()
+    .setColor("#ff0000")
+    .setTitle("Someone was timed out for spamming!")
+    .setDescription("Go and investigate immediately")
+    .addField("User", message.author + "\n(" + userMessage.displayName + ")")
+    .addField("Channel", message.channel)
+    .addField("Discord server", message.guild.name);
+    cyber.send(embedTimeout);
+    hyper.send(embedTimeout);
+    ibrahim.send(embedTimeout);
+    userMessage.addRole(muteroleid);
+    message.channel.startTyping(100000);
+    setTimeout( function() {message.channel.stopTyping(true);}, 5000)
+    message.channel.send("***" + userMessage.displayName + " has been muted***").then(msg => {
+      setTimeout( function() {msg.edit("***" + userMessage.displayName + " has been unmuted***").then(t => {t.delete(5000)})}, 10000)
+    })
+    setTimeout( function() {
+      userMessage.removeRole(muteroleid);
+    }, 10000)
+  } 
+  if(message.content.startsWith("-test")) {
+  message.channel.send(sentMessage[message.author.id].sent);
+  }
+  
+});
+
+mybot.login(process.env.lolbottoken); 
